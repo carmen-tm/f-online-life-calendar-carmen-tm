fc98c28a32961ba2da7cfae4ed8ec083b4815b76
carmen-tm <carmentorremolina@gmail.com>
2019-06-27 15:09:11 +0200

    Start basic components structure

---
+++ src/containers/CalendarPage/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import 'styles.scss';
+
+const CalendarPage = props => {
+	return <div>CalendarPage</div>;
+};
+
+export default CalendarPage;
---
+++ src/containers/EditionPage/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import 'styles.scss';
+
+const EditionPage = props => {
+	return <div>EditionPage</div>;
+};
+
+export default EditionPage;
