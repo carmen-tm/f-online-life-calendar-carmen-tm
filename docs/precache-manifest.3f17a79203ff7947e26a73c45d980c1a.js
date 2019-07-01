98f3fdd4e03ec4e3a7a4b5dae57939889ef4d100
carmen-tm <carmentorremolina@gmail.com>
2019-07-01 17:23:12 +0200

    Update production version

---
+++ docs/precache-manifest.3f17a79203ff7947e26a73c45d980c1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbb53d1c7eb5b19c0a1f9d6b831dd5f6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fa63041c31c5907451f3",
+    "url": "./static/css/main.4ac3ebce.chunk.css"
+  },
+  {
+    "revision": "bc3942e5804c46aea9c4",
+    "url": "./static/js/2.44228668.chunk.js"
+  },
+  {
+    "revision": "fa63041c31c5907451f3",
+    "url": "./static/js/main.2326569d.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "8e8d71852b8be340e21841245dedbd7a",
+    "url": "./static/media/noun_Happy_2257035.8e8d7185.svg"
+  },
+  {
+    "revision": "9a81e82a68c2c65cfebc828427bff63d",
+    "url": "./static/media/noun_cry_2257022.9a81e82a.svg"
+  }
+]);
